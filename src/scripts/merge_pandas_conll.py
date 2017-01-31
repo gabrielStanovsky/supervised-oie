f1494bf24461082800d942a715984c6a5ef99bc3
gabis <gabriel.satanovsky@gmail.com>
2017-01-31 23:09:47 +0200

    wip

---
+++ src/scripts/merge_pandas_conll.py
@@ -0,0 +1,19 @@
+""" Usage:
+    merge_pandas_conll --out=OUTPUT_FN <filenames>...
+
+Merge a list of data frames in csv format and print to output file.
+"""
+from docopt import docopt
+import pandas as pd
+import logging
+logging.basicConfig(level = logging.DEBUG)
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    logging.debug(args)
+    input_fns = args["<filenames>"]
+    out_fn = args["--out"]
+    pd.concat([pd.read_csv(fn,
+                           sep = '\t',
+                           header = 0)
+               for fn in input_fns]).
