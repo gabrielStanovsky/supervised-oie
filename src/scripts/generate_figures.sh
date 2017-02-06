4a2024432c9cd0be63cc87f7b252018b812cdbc7
gabis <gabriel.satanovsky@gmail.com>
2017-02-06 12:03:28 +0200

    cleaning

--- src/scripts/generate_figures.sh
+++ src/scripts/generate_figures.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 set -e
 pushd ../supervised-oie-benchmark/
-# # Newswire figure
-# DIR="../evaluations/figures/newswire"
-# echo "Creating Newswire figure in ${DIR}..."
-# mkdir -p $DIR
-# rm -f $DIR/*
-# python benchmark.py --gold=./oie_corpus/newswire/propbank.test.oie.orig\
-#        --out=$DIR/ClausIE.dat --tabbed=./systems_output/test/newswire/clausie_propbank_test.txt
-# python benchmark.py --gold=./oie_corpus/newswire/propbank.test.oie.orig\
-#        --out=$DIR/OpenIE-4.dat --tabbed=./systems_output/test/newswire/openie4_propbank_test.txt
-# python benchmark.py --gold=./oie_corpus/newswire/propbank.test.oie.orig\
-#        --out=$DIR/PropS.dat --tabbed=./systems_output/test/newswire/props_propbank_test.txt
-# python benchmark.py --gold=./oie_corpus/newswire/propbank.test.oie.orig\
-#        --out=$DIR/RnnOIE_in_domain.dat --tabbed=../evaluations/extractions/newswire_in_domain.txt
-# python benchmark.py --gold=./oie_corpus/newswire/propbank.test.oie.orig\
-#        --out=$DIR/RnnOIE_out_of_domain.dat --tabbed=../evaluations/extractions/wiki_out_of_domain.txt
-# python pr_plot.py --in=${DIR} --out=${DIR} --outputtype=png
-
-# # Wiki figure
-# DIR="../evaluations/figures/wiki"
-# echo "Creating WIKI figure in ${DIR}..."
-# mkdir -p $DIR
-# rm -f $DIR/*
-# python benchmark.py --gold=./oie_corpus/wiki/wiki1.test.oie.orig\
-#        --out=$DIR/ClausIE.dat --tabbed=./systems_output/test/wiki/clausie_wiki_test.txt
-# python benchmark.py --gold=./oie_corpus/wiki/wiki1.test.oie.orig\
-#        --out=$DIR/OpenIE-4.dat --tabbed=./systems_output/test/wiki/openie4_wiki_test.txt
-# python benchmark.py --gold=./oie_corpus/wiki/wiki1.test.oie.orig\
-#        --out=$DIR/PropS.dat --tabbed=./systems_output/test/wiki/props_wiki_test.txt
-# python benchmark.py --gold=./oie_corpus/wiki/wiki1.test.oie.orig\
-#        --out=$DIR/RnnOIE_in_domain.dat --tabbed=../evaluations/extractions/wiki_in_domain.txt
-# python benchmark.py --gold=./oie_corpus/wiki/wiki1.test.oie.orig\
-#        --out=$DIR/RnnOIE_out_of_domain.dat --tabbed=../evaluations/extractions/newswire_out_of_domain.txt
-# python pr_plot.py --in=${DIR} --out=${DIR} --outputtype=png
-
 
 # Joint figure
 DIR="../evaluations/figures/joint"
@@ -51,19 +17,13 @@ python benchmark.py --gold=./oie_corpus/test.oie.orig\
        --out=$DIR/RnnOIE.dat --tabbed=../evaluations/extractions/joint.txt
 python pr_plot.py --in=${DIR} --out=${DIR} --outputtype=png
 
-# Joint figure -- only core
-DIR="../evaluations/figures/binary_test"
-echo "Creating JOINT (binary test) figure in ${DIR}..."
+# Joint figure -- only in test
+DIR="../evaluations/only_in_test"
+echo "Creating OIT  figure in ${DIR}..."
 mkdir -p $DIR
-rm -f $DIR/*
-python benchmark.py --gold=./oie_corpus/test.oie.orig\
-       --out=$DIR/ClausIE.dat --tabbed=./systems_output/test/binary_test/clausie_test.txt
-python benchmark.py --gold=./oie_corpus/test.oie.orig\
-       --out=$DIR/OpenIE-4.dat --tabbed=./systems_output/test/binary_test/openie4_test.txt
-python benchmark.py --gold=./oie_corpus/test.oie.orig\
-       --out=$DIR/PropS.dat --tabbed=./systems_output/test/binary_test/props_test.txt
-python benchmark.py --gold=./oie_corpus/test.oie.orig\
-       --out=$DIR/RnnOIE.dat --tabbed=../evaluations/extractions/binary_test/joint.txt
+rm -f $DIR/*.dat
+python benchmark.py --gold=../evaluations/only_in_test/test.oie.filter\
+       --out=$DIR/OIT.dat --tabbed=../evaluations/only_in_test/joint.filter
 python pr_plot.py --in=${DIR} --out=${DIR} --outputtype=png
 
 
