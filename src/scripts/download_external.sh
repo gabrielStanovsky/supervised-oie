d4b130c02dcf11377621efd6affd0be923a08ef4
gabis <gabriel.satanovsky@gmail.com>
2017-06-21 17:40:12 +0300

    downloading external

--- src/scripts/download_external.sh
+++ src/scripts/download_external.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ## Download external data required for this project
 set -e
-# Download and unpack oie corpus
-wget https://github.com/gabrielStanovsky/oie-benchmark/blob/master/snapshot_oie_corpus.tar.gz
-tar xvzf snapshot_oie_corpus.tar.gz
+if [ ! -f "snapshot_oie_corpus.tar.gz" ]
+then
+    echo "Downloading and unpacking OIE corpus..."
+      # Download and unpack oie corpus
+    wget https://github.com/gabrielStanovsky/oie-benchmark/raw/master/snapshot_oie_corpus.tar.gz
+    tar xvzf snapshot_oie_corpus.tar.gz
+    echo "Done!"
+fi
+
+# Done
+echo "Succesfully finished downloading external data"
